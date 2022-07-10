c922cfe9b923f31eb78e8f86d730b90d5762d885
shihab aldeen <shihab201123x@gmail.com>
2022-07-10 05:20:13 +0300

    lasst

--- project/urls.py
+++ project/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/',include('accounts.urls',namespace='accounts')),
     path('admin/', admin.site.urls),
+    path('', include('job.urls',namespace="jobs")),
     path('job/', include('job.urls',namespace="jobs")),
     path('contact/', include('contact.urls',namespace="contact")),
     path('api-auth/', include('rest_framework.urls')),
